ed46e6856762d32eba8a7096b236ca72374bf36f
Michael Davis <m.r.davis@me.com>
2010-10-10 18:05:19 +0000

    Reference the version.txt file from the setup.py
    git-svn-id: http://svn.plone.org/svn/collective/Products.PloneSurvey/trunk@145938 db7f04ef-aaf3-0310-a811-c281ed44c4ad

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '1.3.0'
+version = open(os.path.join("Products", "PloneSurvey", "version.txt")).read().strip()
 
 setup(name='Products.PloneSurvey',
       version=version,
