a7b21e7973008d909f0ca5de63b87e91006cbf82
Michael Davis <m.r.davis@me.com>
2010-10-31 16:32:10 +0000

    Hook the layers up properly
    git-svn-id: http://svn.plone.org/svn/collective/Products.PloneSurvey/trunk@225790 db7f04ef-aaf3-0310-a811-c281ed44c4ad

--- Products/PloneSurvey/browser/interfaces.py
+++ Products/PloneSurvey/browser/interfaces.py
@@ -1,6 +1,6 @@
 from zope.interface import Interface
 
-class IThemeSpecific(Interface):
+class IPloneSurveySpecific(Interface):
     """Marker interface that defines a Zope 3 browser layer.
        If you need to register a viewlet only for the
        "PloneSurvey" theme, this interface must be its layer
