838790a2eef1e1b5d33903ebf735800e7f88dda7
Yuri <yurj@alfa.it>
2010-11-10 16:02:24 +0000

    refactoring of the
    git-svn-id: http://svn.plone.org/svn/collective/Products.PloneSurvey/trunk@226502 db7f04ef-aaf3-0310-a811-c281ed44c4ad

--- Products/PloneSurvey/skins/plone_survey/mostra_quest.py
+++ Products/PloneSurvey/skins/plone_survey/mostra_quest.py
@@ -4,46 +4,49 @@
 ##bind namespace=
 ##bind script=script
 ##bind subpath=traverse_subpath
-##parameters=type='pdf'
+##parameters=print_type='pdf'
 ##title=
 ##
 
 #BBB to rename in show_survey o similar, and convert to a browser view
-if type <> 'html':
- print '<html>'
- print """<head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+
+def compose(text, phrase):
+    return text + phrase
+
+result = ''
+
+if print_type <> 'html':
+ compose(result,'<html>')
+ compose(result,"""<head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
 
 <style>
 #BBB import the real site name
 @import "http://mysite/print.css";
 </style> 
-</head>"""
+</head>""")
 else:
- print """<style>
+ compose(result,"""<style>
 @import "http://mysite/print_web.css";
 </style> 
-</head>"""
+</head>""")
+if print_type <> 'html':
+ compose(result,'<body>')
 
-
-if type <> 'html':
- print '<body>'
-
-print context.getPrint_header().decode("utf-8")
+compose(result,context.getPrint_header().decode("utf-8"))
 
 for i in context.objectIds():
  survey = context[i]
  iden = survey.getId()
- if iden not in ['acl_users','riepilogo']:
+ if iden not in ['acl_users','summary']:
+  questions = survey.getQuestions
+  compose(result,survey.print_page(questions=questions, quest=survey))
 
-  questions = quest.getQuestions
-  print survey.print_page(questions=questions, quest = survey)
+if print_type <> 'html':
+ compose(result,'</body>')
+ compose(result,'</html>')
 
-if type <> 'html':
- print '</body>'
- print '</html>'
-
-if type == 'html':
- return context.print_page_html(testo = printed)
+if print_type == 'html':
+ return context.print_page_html(testo = result)
 else:
  # need an external method called html2pdf which sends the html to an html2pdf util)
- return context.html2pdf(html=printed)
+ return context.html2pdf(html=result)
