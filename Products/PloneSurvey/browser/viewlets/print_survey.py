5095c1ce358b94c2e1188db4fc7978f2cc157a94
Michael Davis <m.r.davis@me.com>
2011-04-30 10:24:12 +0000

    Change the view to return a pdf. Defined filename is being overridden and only the title is displayed so far.
    git-svn-id: http://svn.plone.org/svn/collective/Products.PloneSurvey/trunk@238378 db7f04ef-aaf3-0310-a811-c281ed44c4ad

--- Products/PloneSurvey/browser/viewlets/print_survey.py
+++ Products/PloneSurvey/browser/viewlets/print_survey.py
@@ -1,7 +1,14 @@
+from zope.app.pagetemplate.viewpagetemplatefile import ViewPageTemplateFile
+from z3c.rml.rml2pdf import parseString
 from Products.Five.browser import BrowserView
 
 class PrintSurveyView(BrowserView):
     """A printable view of a survey, rendered as a pdf"""
+
+    def __call__(self):
+        self.request.response.setHeader('content-type', 'application/pdf')
+        rml_doc = ViewPageTemplateFile('print_survey.pt')(self)
+        return parseString(rml_doc.encode('utf-8')).read()
 
     def name(self):
         return self.context.Title()
