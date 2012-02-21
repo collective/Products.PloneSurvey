067b3af773aed2c43f5ee968a489939a346bbbbf
Ralph Jacobs <ralph@fourdigits.nl>
2012-02-21 15:02:37 +0000

    PEP8 and removed unused imports
    git-svn-id: http://svn.plone.org/svn/collective/Products.PloneSurvey/trunk@248534 db7f04ef-aaf3-0310-a811-c281ed44c4ad

--- Products/PloneSurvey/content/SurveyMatrixQuestion.py
+++ Products/PloneSurvey/content/SurveyMatrixQuestion.py
@@ -1,10 +1,8 @@
-import string
 from AccessControl import ClassSecurityInfo
 from zope.interface import implements
 
 from Products.Archetypes.atapi import *
 from Products.ATContentTypes.content.base import registerATCT
-from Products.CMFCore.utils import getToolByName
 
 from Products.PloneSurvey import permissions
 from Products.PloneSurvey.config import PROJECTNAME
@@ -12,6 +10,7 @@ from Products.PloneSurvey.interfaces import IPloneSurveyQuestion
 
 from BaseQuestion import BaseQuestion
 from schemata import SurveyMatrixQuestionSchema
+
 
 class SurveyMatrixQuestion(BaseQuestion):
     """A question in a matrix within a survey"""
@@ -24,6 +23,7 @@ class SurveyMatrixQuestion(BaseQuestion):
     security = ClassSecurityInfo()
 
     security.declareProtected(permissions.View, 'validateAnswer')
+
     def validateAnswer(self, value, state):
         """Validate the question"""
         try:
@@ -40,15 +40,17 @@ class SurveyMatrixQuestion(BaseQuestion):
         except ValueError:
             pass
         if self.getRequired() and not value and not value == 0:
-           return 1
+            return 1
         self.addAnswer(value)
 
     security.declareProtected(permissions.View, 'getRequired')
+
     def getRequired(self):
         """Return 1 or 0 depending on if a null value exists"""
         return self.aq_parent.getRequired()
 
     security.declareProtected(permissions.View, 'getAggregateAnswers')
+
     def getAggregateAnswers(self):
         """Return a mapping of aggregrate answer values,
         suitable for a histogram"""
@@ -60,7 +62,8 @@ class SurveyMatrixQuestion(BaseQuestion):
             aggregate_answers[option] = 0
         for k, answer in self.answers.items():
             if answer['value']:
-                if isinstance(answer['value'], str) or isinstance(answer['value'], int):
+                if isinstance(answer['value'],
+                              str) or isinstance(answer['value'], int):
                     try:
                         aggregate_answers[answer['value']] += 1
                     except KeyError:
@@ -74,40 +77,43 @@ class SurveyMatrixQuestion(BaseQuestion):
         return aggregate_answers
 
     security.declareProtected(permissions.View, 'getPercentageAnswers')
+
     def getPercentageAnswers(self):
         """Return a mapping of aggregrate answer values,
         suitable for a barchart"""
         max = 0
         aggregate_answers = self.getAggregateAnswers()
-        for k,v in aggregate_answers.items():
+        for k, v in aggregate_answers.items():
             if v > max:
                 max = v
         pct_aggregate_answers = {}
-        for k,v in aggregate_answers.items():
+        for k, v in aggregate_answers.items():
             if v == 0:
                 value = 0
             else:
-                value = v/float(max)
+                value = v / float(max)
             pct_aggregate_answers[k] = int(value * 100)
         return pct_aggregate_answers
 
     security.declareProtected(permissions.View, 'getPercentages')
+
     def getPercentages(self):
         """Return a mapping of percentages for each answer"""
         total = 0
         aggregate_answers = self.getAggregateAnswers()
-        for k,v in aggregate_answers.items():
+        for k, v in aggregate_answers.items():
             total = v + total
         pct_answers = {}
-        for k,v in aggregate_answers.items():
+        for k, v in aggregate_answers.items():
             if v == 0:
                 value = 0
             else:
-                value = v/float(total)
+                value = v / float(total)
             pct_answers[k] = int(value * 100)
         return pct_answers
 
     security.declareProtected(permissions.View, 'getAnswerOptionsWeights')
+
     def getAnswerOptionsWeights(self):
         return self.aq_parent.getAnswerOptionsWeights()
 
