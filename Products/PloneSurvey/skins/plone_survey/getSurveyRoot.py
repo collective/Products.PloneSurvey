b5043373e4d00e592dfae5915a2ababd27f7f818
Michael Davis <m.r.davis@me.com>
2013-01-08 13:10:04 +0000

    The survey navigation portlet would need to be completely rewritten, so delete for now, which means the getSurveyRoot script can also be deleted.
    git-svn-id: http://svn.plone.org/svn/collective/Products.PloneSurvey/trunk@252032 db7f04ef-aaf3-0310-a811-c281ed44c4ad

--- Products/PloneSurvey/skins/plone_survey/getSurveyRoot.py
+++
@@ -1,9 +0,0 @@
-PARENTS = context.REQUEST.PARENTS
-
-if context.meta_type == 'Survey':
-    return context
-for parent in PARENTS:
-    if parent.meta_type == 'Survey':
-        return parent
-
-return
