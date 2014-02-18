bee3097b106ef31f098734e3b3724ecd923eb2ba
Andrea Cecchi <andrea.cecchi85@gmail.com>
2014-02-18 11:12:30 +0100

    fixes to translations and i18ndude script

--- Products/PloneSurvey/locales/usei18ndude.sh
+++ Products/PloneSurvey/locales/usei18ndude.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
-i18ndude rebuild-pot --pot plonesurvey.pot --create plonesurvey ..
-i18ndude sync --pot plonesurvey.pot plonesurvey-??.po
+DOMAIN='plonesurvey'
 
+i18ndude rebuild-pot --pot ${DOMAIN}.pot --create ${DOMAIN} ..
+i18ndude rebuild-pot --pot plone.pot --create plone ../profiles/default/types
+
+i18ndude sync --pot ${DOMAIN}.pot */LC_MESSAGES/${DOMAIN}.po
+i18ndude sync --pot plone.pot */LC_MESSAGES/plone.po
 # Updating "plone" domain
 i18ndude sync --pot plonesurvey-plone.pot plonesurvey-plone-??.po
