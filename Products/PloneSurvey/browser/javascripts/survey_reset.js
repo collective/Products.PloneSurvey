34b54f696a8b1a7f3c93748a5c4b861646839464
Stephan Geulette <s.geulette@imio.be>
2015-10-17 12:21:33 +0300

    Updated javascript to avoid definition error

--- Products/PloneSurvey/browser/javascripts/survey_reset.js
+++ Products/PloneSurvey/browser/javascripts/survey_reset.js
@@ -1,11 +1,9 @@
-(function($) {
-    $(document).ready(function(){
-        jq('#survey_reset_form').submit(function() {
-        	if (confirm("Are you sure you want to reset the survey?")) {
-                return true;
-            } else {
-            	return false;
-            };
-	    });
+$(document).ready(function(){
+	$('#survey_reset_form').submit(function() {
+		if (confirm("Are you sure you want to reset the survey?")) {
+			return true;
+		} else {
+			return false;
+		};
 	});
-})(jQuery);
+});
