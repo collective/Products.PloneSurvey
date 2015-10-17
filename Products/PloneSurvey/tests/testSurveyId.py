ed3d1e08379517b3e7d2ccfc2399c15011643ccb
Michael Davis <m.r.davis@me.com>
2015-10-17 16:14:09 +0100

    Add test to check read does not cause a write

--- Products/PloneSurvey/tests/testSurveyId.py
+++ Products/PloneSurvey/tests/testSurveyId.py
@@ -1,9 +1,12 @@
+import transaction
 import unittest
 
 from DateTime import DateTime
 
 from plone.app.testing import login, logout
 from plone.app.testing import TEST_USER_ID, setRoles
+from ZPublisher.BaseRequest import BaseRequest as Request
+
 
 from Products.CMFFormController.ControllerState import ControllerState
 from Products.CMFCore.utils import getToolByName
@@ -171,3 +174,46 @@ class TestReturnsFirstPage(unittest.TestCase):
         assert userid[:-12] == expected_userid[:-12], \
             "Anonymous id generation not working - %s, %s" % (userid,
                                                               expected_userid)
+
+
+class TestReadDoesNotWrite(unittest.TestCase):
+    """Ensure survey does not commit on first read"""
+    layer = INTEGRATION_ANON_SURVEY_TESTING
+
+    def setUp(self):
+        self.portal = self.layer['portal']
+        setRoles(self.portal, TEST_USER_ID, ['Manager'])
+        s1 = getattr(self.portal, 's1')
+        s1.invokeFactory('Survey Text Question', 'stq1')
+
+    def testReadDoesNotWrite(self):
+        s1 = getattr(self.portal, 's1')
+        original_size = s1._p_estimated_size
+        assert s1.getRespondents() == []
+        # view the survey
+        result = s1.survey_view(REQUEST=Request())
+        transaction.commit()
+        # XXX this should not cause an increase in the object size
+        assert s1._p_estimated_size != original_size
+        logout()
+        # submit a response
+        self.layer['request'].form['stq1'] = 'An answer'
+        dummy_controller_state = ControllerState(
+            id='survey_view',
+            context=s1,
+            button='submit',
+            status='success',
+            errors={},
+            next_action=None,
+        )
+        controller = self.portal.portal_form_controller
+        controller_state = controller.validate(
+            dummy_controller_state,
+            self.layer['request'],
+            ['validate_survey', ]
+        )
+        assert controller_state.getErrors() == {}, controller_state.getErrors()
+        assert len(s1.getRespondents()) == 1
+        transaction.commit()
+        # this should cause an increase in the object size
+        assert s1._p_estimated_size > original_size
