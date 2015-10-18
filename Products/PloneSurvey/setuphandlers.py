f6f9557170ff0ba13df0de58d2289f6af38d0f22
Stephan Geulette <s.geulette@imio.be>
2015-10-18 12:13:50 +0300

    Assign correctly the registry

--- Products/PloneSurvey/setuphandlers.py
+++ Products/PloneSurvey/setuphandlers.py
@@ -19,10 +19,11 @@ def importVarious(context):
     registry = getUtility(IRegistry)
     navigation_settings = registry.forInterface(INavigationSchema, prefix='plone')
     if 'Survey' not in navigation_settings.displayed_types:
-        navigation_settings.displayed_types = list(navigation_settings.displayed_types).extend(['Survey'])
+        tmp = tuple(navigation_settings.displayed_types)
+        navigation_settings.displayed_types = tmp + ('Survey',)
     types_settings = registry.forInterface(ITypesSchema, prefix='plone')
     if 'Survey' not in types_settings.default_page_types:
-        types_settings.default_page_types = list(types_settings.default_page_types).extend(['Survey'])
+        types_settings.default_page_types += [u'Survey']
 
 
 def nullStep(context, logger=None):
